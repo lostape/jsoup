fb068c05d31f5b5815a65f058455fe026408198d
lostape <mikmister02@gmail.com>
2013-03-04 11:03:11 -0700

    ScriptEndTag Tests

--- src/main/java/test/JsoupWhiteboxTests.java
+++ src/main/java/test/JsoupWhiteboxTests.java
@@ -74,5 +74,72 @@ public class JsoupWhiteboxTests {
 		//Jsoup replaces null character with replacement char
 		assertEquals("", d.body().child(0).text());
 	}
+	
+	@Test
+	public void ScriptDataNormalTest() {
+		String html = "<script><normaltag>sometext</normaltag></script>";
+		Document d = Jsoup.parse(html);
+
+		assertEquals("<normaltag>sometext</normaltag>", d.head().child(0).html());
+	}
+	
+	@Test
+	public void ScriptDataNullCharTest() {
+		String html = "<script>NullChar\0</script>";
+		Document d = Jsoup.parse(html);
+		//Jsoup replaces null character with replacement char
+		assertEquals("NullChar\uFFFD", d.head().child(0).html());
+	}
+	
+	@Test
+	public void ScriptDataEOFTest() {
+		char eof = (char) -1;
+		String html = "<script>"+eof+"Stuff after will not be parsed</script>";
+		Document d = Jsoup.parse(html);
+
+		assertEquals("", d.head().child(0).html());
+	}
+	
+	@Test
+	public void ScriptDataImproperEndTagTest() {
+		String html = "<script>Fake endtag script, no space allowed </ script></script>";
+		Document d = Jsoup.parse(html);
+
+		assertEquals("Fake endtag script, no space allowed </ script>", d.head().child(0).html());
+	}
+	
+	@Test
+	public void ScriptDataSelfClosingTagTest() {
+		String html = "<script>Improper syntax for self closing tag</script/>";
+		Document d = Jsoup.parse(html);
+
+		assertEquals("Improper syntax for self closing tag", d.head().child(0).html());
+		fail();
+	}
+	
+	@Test
+	public void ScriptEndTagWithAttributeTest() {
+		String html = "<script>Hello</script attr=\"1\"/>";
+		Document d = Jsoup.parse(html);
+
+		assertEquals("",d.head().child(0).attr("attr"));
+		assertEquals("Hello", d.head().child(0).html());
+	}
+	
+	@Test
+	public void ScriptEndTagEndsAbruptlyTest() {
+		String html = "<script>Hello</script";
+		Document d = Jsoup.parse(html);
+
+		assertEquals("Hello</script", d.head().child(0).html());
+	}
+	
+	@Test
+	public void ScriptEndTagTest() {
+		String html = "<script>Hello</script89764>";
+		Document d = Jsoup.parse(html);
+
+		assertEquals("Hello</script89764>", d.head().child(0).html());
+	}
 
 }
